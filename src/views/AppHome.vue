2945c3415f2b2dfe84fec59262b54fe9654e021d
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-06-06 16:35:28 +0200

    edit homepage

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -7,14 +7,32 @@ export default {
 
 
 <template>
-    <main class="min-vh-100">
-        <div class=" text-center text-light">
-            RPG GENERATOR HOME
+    <main class="vh-100">
+        <div class="jumbotron p-5 mb-4 rounded-3">
+            <div class="container-fluid py-5">
+                <h1 class="display-5 fw-bold text-warning">RPG Generator</h1>
+                <p class="col-md-8 fs-4 text-center mx-auto text-danger">
+                    Using a series of utilities, you can create this jumbotron, just
+                    like the one in previous versions of Bootstrap. Check out the
+                    examples below for how you can remix and restyle it to your liking.
+                </p>
+                <button class="btn btn-warning btn-lg fw-bold" type="button">
+                    Go to HELL
+                </button>
+            </div>
         </div>
+
     </main>
 </template>
 
 
-<style lang="">
-
+<style>
+.jumbotron {
+    height: 100%;
+    background-image: url('/public/flames-bg.jpg');
+    background-position: 30%;
+    background-repeat: no-repeat;
+    background-size: cover;
+    text-align: center;
+}
 </style>
